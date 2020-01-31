7b653b47faa39eb601c04baf605adacf2e8924fa
akuryou <akuryou@byteweaver.org>
2020-01-31 22:47:43 +0100

    add controller movement to player entity

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -4,8 +4,21 @@ class Player extends Phaser.GameObjects.Container {
     scene.add.existing(this);
 
     this.playerId = playerId;
+    this.speed = 0.2;
+
     this.playerSprite = scene.add.image(0, 0, 'PlayerSprite');
     this.add(this.playerSprite);
+  }
+
+  preUpdate(time, delta) {
+    let pad = this.scene.input.gamepad.getPad(this.playerId);
+    if(!pad) {
+      return;
+    }
+
+    this.x += delta * this.speed * pad.axes[0].getValue();
+    this.y += delta * this.speed * pad.axes[1].getValue();
+    //sprite.flipX = (axisH > 0);
   }
 }
 
