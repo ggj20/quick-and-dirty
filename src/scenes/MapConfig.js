8cb7709bbff5dd5a471d880ee6fda9b95ee0dbb0
Fabian Netzler <fnetzler@gmail.com>
2020-02-01 22:02:37 +0100

    added more damage areas

--- src/scenes/MapConfig.js
+++ src/scenes/MapConfig.js
@@ -8,10 +8,29 @@ var mapAreas = {
         {xSource : 1005, xLength: 130, ySource : 100, yLength: 25}, // Room 2
         {xSource : 1450, xLength: 115, ySource : 90, yLength: 30}, // Room 2
         {xSource : 1685, xLength: 25, ySource : 255, yLength: 150}, // Room 2
-        {xSource : 1500, xLength: 200, ySource : 475, yLength: 30} // Room 2
+        {xSource : 1500, xLength: 200, ySource : 475, yLength: 30}, // Room 2
+        {xSource : 700, xLength: 145, ySource : 570, yLength: 25}, //Room 3
+        {xSource : 235, xLength: 20, ySource : 840, yLength: 120}, //Room 3
+        {xSource : 265, xLength: 175, ySource : 970, yLength: 20}, // Room 3
+        {xSource : 920, xLength: 20, ySource : 930, yLength: 60}, //Room 3
+        {xSource : 1005, xLength: 20, ySource : 585, yLength: 75}, // Room 4
+        {xSource : 1025, xLength: 40, ySource : 570, yLength: 25}, // Room 4
+        {xSource : 1500, xLength: 185, ySource : 570, yLength: 25}, // Room 4
+        {xSource : 1685, xLength: 20, ySource : 585, yLength: 235}, // Room 2
+        {xSource : 1255, xLength: 65, ySource : 960, yLength: 25} // Room 2
     ],
 electricityAreas: [],
-fireAreas:[],
-holeAreas:[]};
+fireAreas:[
+    {xSource : 270, xLength: 635, ySource : 125, yLength: 350}, //Room 1
+    {xSource : 1040, xLength: 635, ySource : 125, yLength: 350}, //Room 2
+    {xSource : 270, xLength: 635, ySource : 605, yLength: 350}, //Room 3
+    {xSource : 1040, xLength: 635, ySource : 605, yLength: 350} //Room 4
+],
+holeAreas:[
+    {xSource : 270, xLength: 635, ySource : 125, yLength: 350}, //Room 1
+    {xSource : 1040, xLength: 635, ySource : 125, yLength: 350}, //Room 2
+    {xSource : 270, xLength: 635, ySource : 605, yLength: 350}, //Room 3
+    {xSource : 1040, xLength: 635, ySource : 605, yLength: 350} //Room 4
+]};
 
 export default mapAreas;
