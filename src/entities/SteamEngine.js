e586bc6acaf507b1c8080410209c523ef41c6ff4
Richard <richard@stromer.org>
2020-02-01 23:43:59 +0100

    fine tune position and size of steam engine

--- src/entities/SteamEngine.js
+++ src/entities/SteamEngine.js
@@ -1,10 +1,10 @@
 export default class SteamEngine extends Phaser.GameObjects.Container {
   constructor(scene) {
-    super(scene, 108, 352);
+    super(scene, 106, 352);
 
-    const graphics = scene.make.graphics(207, 376);
-    graphics.fillStyle(0xffff00, 0.5);
-    graphics.fillRect(0, 0, 205, 376);
+    const graphics = scene.make.graphics(208, 376);
+    graphics.fillStyle(0xffff00, 0.8);
+    graphics.fillRect(0, 0, 208, 376);
     this.add(graphics);
 
     scene.add.existing(this);
