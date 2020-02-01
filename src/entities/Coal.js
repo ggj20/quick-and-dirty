a650f232f1e54e4a1a8bf209badd71a675378fce
akuryou <akuryou@byteweaver.org>
2020-02-02 00:34:17 +0100

    add phys enabler for coal

--- src/entities/Coal.js
+++ src/entities/Coal.js
@@ -2,7 +2,8 @@ import Tool from './Tool';
 
 class Coal extends Tool {
     constructor(scene, x, y) {
-        super(scene, x, y, 'CoalSprite', 'COAL');
+      super(scene, x, y, 'CoalSprite', 'COAL');
+      scene.physics.world.enable(this);
     }
     use() {
       return;
