c0a4d97013220842dbbdf428c9f4bf8d17acd004
akuryou <akuryou@byteweaver.org>
2020-02-01 12:10:28 +0100

    add start text

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -12,6 +12,14 @@ class TitleScene extends Phaser.Scene {
   }
 
   create() {
+    this.make.text({
+      x: this.game.config.width/2,
+      y: 100,
+      text: "Press any button to start",
+      style: { font: "65px Arial", fill: "#CCC", align: "center"},
+      origin: { x: 0.5, y: 0.5 },
+      add: true
+    });
     this.scene.start('GameScene');
   }
 }
