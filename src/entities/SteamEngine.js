59312d3b9780968007cd80747816a3aa1dae2c2d
Richard <richard@stromer.org>
2020-02-01 23:41:23 +0100

    draw rect around steam engine asset

--- src/entities/SteamEngine.js
+++ src/entities/SteamEngine.js
@@ -1,1 +1,12 @@
-export default class SteamEngine extends Phaser.GameObjects.Container {}
+export default class SteamEngine extends Phaser.GameObjects.Container {
+  constructor(scene) {
+    super(scene, 108, 352);
+
+    const graphics = scene.make.graphics(207, 376);
+    graphics.fillStyle(0xffff00, 0.5);
+    graphics.fillRect(0, 0, 205, 376);
+    this.add(graphics);
+
+    scene.add.existing(this);
+  }
+}
