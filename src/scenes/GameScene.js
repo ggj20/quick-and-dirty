94b3d3e4530698f67fc171cf9317db079c343c47
Thinh <cantle@hotmail.de>
2020-02-01 19:19:44 +0100

    fitting wall hitbox

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -22,15 +22,18 @@ class GameScene extends Phaser.Scene {
 
     this.shipSprite = this.add.image(0, 0, 'ShipSprite').setOrigin(0, 0);
 
-    for(let i = 0; i < this.game.settings.playerCount; i++) {
-      new Player(this, [200, 200], [500, 500], i, this.toolGroup, this.damageGoup, this.zoneGroup);
-    }
+
+    new Player(this, [330, 180], [500, 320], 0, this.toolGroup, this.damageGoup, this.zoneGroup);
+    //new Player(this, [905, 180], [500, 320], 1, this.toolGroup, this.damageGoup, this.zoneGroup);
+    //new Player(this, [330, 565], [500, 320], 2, this.toolGroup, this.damageGoup, this.zoneGroup);
+	//new Player(this, [905, 565], [500, 320], 3, this.toolGroup, this.damageGoup, this.zoneGroup);
+
 
     //debug objects
-    this.hammer = new Hammer(this, 200, 200,'HammerSprite');
+    this.hammer = new Hammer(this, 400, 400,'HammerSprite');
     this.toolGroup.add(this.hammer)
     this.physics.world.enable(this.hammer);
-    this.leak = new Leak(this, 220, 220,'LeakSprite');
+    this.leak = new Leak(this, 500, 300,'LeakSprite');
     this.physics.world.enable(this.leak);
     this.damageGoup.add(this.leak);
     this.tube1 = new ItemTube(this, {x: 810, y:370}, {x:930, y:370}, this.zoneGroup); 
