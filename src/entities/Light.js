59836c4dad2e6dde536d57456aba7e765809f8ae
Fabian Netzler <fnetzler@gmail.com>
2020-02-02 15:28:54 +0100

    added advanced light mechanics

--- src/entities/Light.js
+++ src/entities/Light.js
@@ -12,7 +12,7 @@ class Light extends Phaser.GameObjects.Container {
     if(Math.random() < this.scene.game.state.voltage / 100) {
       this.shadow.alpha = 0;
     } else {
-      this.shadow.alpha = 0.9;
+      this.shadow.alpha = 1 - (this.scene.game.state.voltage / 100);
     }
     setTimeout(this.update.bind(this), 1000);
   }
