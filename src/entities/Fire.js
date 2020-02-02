1de5ab0aa02334b727a6e289c73fc11feda86fac
Fabian Netzler <fnetzler@gmail.com>
2020-02-02 14:06:59 +0100

    added fire spreading

--- src/entities/Fire.js
+++ src/entities/Fire.js
@@ -1,9 +1,34 @@
 import Damage from './Damage';
+import mapAreas from '../scenes/MapConfig';
 
 class Fire extends Damage {
-    constructor(scene, x, y) {
+    constructor(scene, x, y, timeOut = 5000) {
         super(scene, x, y, 'FireSpriteSheet', 'EXTINGUISHER', 100,  { start: 0, end: 24 }, { start: 1, end: 24 }, 17);
+        setTimeout(() => this.spreadFire(timeOut), timeOut);
     }
+
+    spreadFire(timeOut) {
+        let chance = Math.random();
+        if (chance <= this.scene.game.settings.fireSpreadChance) {
+            let x = this.x + (Math.random() - 0.5) * 100;
+            let y = this.y + (Math.random() - 0.5) * 100;
+            let newTimeout = timeOut + timeOut * (Math.random() - 0.5) * 0.5;
+            // do area check
+            let areaFound = false;
+            mapAreas.fireAreas.forEach(element => {
+                if (element.xSource < x && (element.xSource+ element.xLength)> x){
+                    if (element.ySource < y && (element.ySource+ element.yLength)> y){
+                        let newFire = new Fire(this.scene,x , y, newTimeout);
+                        this.scene.physics.world.enable(newFire);
+                        newFire.body.setImmovable();
+                        this.scene.damageGoup.add(newFire);
+                    }
+                }
+            });
+        }
+        setTimeout(() => this.spreadFire(timeOut), timeOut);
+    }
+
 }
 
 export default Fire;
