4fad967d129e874824e79217697d2b1ea6f2fadf
akuryou <akuryou@byteweaver.org>
2020-02-02 12:03:30 +0100

    show score on game over screen

--- src/scenes/GameOverScene.js
+++ src/scenes/GameOverScene.js
@@ -11,6 +11,23 @@ class GameOverScene extends Phaser.Scene {
   }
 
   create() {
+    this.make.text({
+      x: this.game.config.width/2,
+      y: 200,
+      text: "Game Over",
+      style: { font: "85px Arial", fill: "#C44", align: "center"},
+      origin: { x: 0.5, y: 0.5 },
+      add: true
+    });
+
+    this.make.text({
+      x: this.game.config.width/2,
+      y: 500,
+      text: "Your Score: " + this.game.state.score,
+      style: { font: "65px Arial", fill: "#CCC", align: "center"},
+      origin: { x: 0.5, y: 0.5 },
+      add: true
+    });
   }
 }
 
