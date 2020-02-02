de2bf8a3bc21083e629517682427edcc17b6fc31
Richard <richard@stromer.org>
2020-02-02 10:37:40 +0100

    reset formatting changes to game scene

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -9,13 +9,12 @@ import Fire from '../entities/Fire';
 import Hole from '../entities/Hole';
 import Electro from '../entities/Electro';
 import ItemTube from '../entities/ItemTube';
-import SteamEngine from '../entities/SteamEngine';
 
 class GameScene extends Phaser.Scene {
   constructor() {
     super({
       key: 'GameScene'
-    });
+    })
   }
 
   preload() {
@@ -27,45 +26,19 @@ class GameScene extends Phaser.Scene {
     this.toolGroup = this.add.group();
     this.zoneGroup = this.add.group();
 
-    this.shipSprite = this.add
-      .image(
-        this.game.config.width / 2,
-        this.game.config.height / 2,
-        'ShipSprite'
-      )
-      .setOrigin(0.5, 0.5);
+    this.shipSprite = this.add.image(this.game.config.width/2, this.game.config.height/2, 'ShipSprite').setOrigin(0.5, 0.5);
 
     this.rooms = [
-      [
-        [275, 128],
-        [630, 350]
-      ],
-      [
-        [1040, 128],
-        [630, 350]
-      ],
-      [
-        [275, 605],
-        [630, 350]
-      ],
-      [
-        [1040, 605],
-        [630, 350]
-      ]
-    ];
+      [[275, 128], [630, 350]],
+      [[1040, 128], [630, 350]],
+      [[275, 605], [630, 350]],
+      [[1040, 605], [630, 350]],
+    ]
 
     this.spawnTools();
 
-    for (let i = 0; i < this.game.settings.playerCount; i++) {
-      new Player(
-        this,
-        this.rooms[i][0],
-        this.rooms[i][1],
-        i,
-        this.toolGroup,
-        this.damageGoup,
-        this.zoneGroup
-      );
+    for(let i = 0; i < this.game.settings.playerCount; i++) {
+      new Player(this, this.rooms[i][0], this.rooms[i][1], i, this.toolGroup, this.damageGoup, this.zoneGroup);
     }
 
     //debug objects
@@ -86,15 +59,10 @@ class GameScene extends Phaser.Scene {
     this.damageGoup.add(this.electro);
 
     // Tubes
-    new ItemTube(this, { x: 880, y: 300 }, { x: 1065, y: 300 }, this.zoneGroup); // Top
-    new ItemTube(this, { x: 590, y: 450 }, { x: 590, y: 630 }, this.zoneGroup); // Left
-    new ItemTube(
-      this,
-      { x: 1355, y: 450 },
-      { x: 1355, y: 630 },
-      this.zoneGroup
-    ); // Right
-    new ItemTube(this, { x: 880, y: 775 }, { x: 1065, y: 775 }, this.zoneGroup); // Bottom
+    new ItemTube(this, {x: 880, y:300}, {x: 1065, y: 300}, this.zoneGroup); // Top
+    new ItemTube(this, {x: 590, y: 450}, {x: 590, y: 630}, this.zoneGroup); // Left
+    new ItemTube(this, {x: 1355, y: 450}, {x: 1355, y: 630}, this.zoneGroup); // Right
+    new ItemTube(this, {x: 880, y: 775}, {x: 1065, y: 775}, this.zoneGroup); // Bottom
 
     // Steam Engine
     new SteamEngine(this, this.zoneGroup);
@@ -102,19 +70,19 @@ class GameScene extends Phaser.Scene {
 
   spawnTools() {
     this.hammer = new Hammer(this, 400, 400);
-    this.toolGroup.add(this.hammer);
+    this.toolGroup.add(this.hammer)
     this.physics.world.enable(this.hammer);
 
-    this.extinguisher = new Extinguisher(this, 400, 400);
-    this.toolGroup.add(this.extinguisher);
+    this.extinguisher = new Extinguisher(this, 300, 400);
+    this.toolGroup.add(this.extinguisher)
     this.physics.world.enable(this.extinguisher);
 
     this.pipeWrench = new PipeWrench(this, 400, 400);
-    this.toolGroup.add(this.pipeWrench);
+    this.toolGroup.add(this.pipeWrench)
     this.physics.world.enable(this.pipeWrench);
 
     this.solderingIron = new SolderingIron(this, 400, 400);
-    this.toolGroup.add(this.solderingIron);
+    this.toolGroup.add(this.solderingIron)
     this.physics.world.enable(this.solderingIron);
   }
 }
