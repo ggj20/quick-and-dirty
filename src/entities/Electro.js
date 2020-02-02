9815864ab7a95bd53ab96123fc00b7459764d326
Fabian Netzler <fnetzler@gmail.com>
2020-02-02 12:04:15 +0100

    Added animations to damages

--- src/entities/Electro.js
+++ src/entities/Electro.js
@@ -2,7 +2,7 @@ import Damage from './Damage';
 
 class Electro extends Damage {
     constructor(scene, x, y) {
-        super(scene, x, y, 'ElectroSprite', 'SOLDERING_IRON', 100);
+        super(scene, x, y, 'ElectroSpriteSheet', 'SOLDERING_IRON', 100,  { start: 0, end: 24}, { start: 0, end: 24 }, 48);
     }
 }
 
--- src/entities/Leak.js
+++ src/entities/Leak.js
@@ -2,7 +2,7 @@ import Damage from './Damage';
 
 class Leak extends Damage {
     constructor(scene, x, y) {
-        super(scene, x, y, 'LeakSprite', 'PIPE_WRENCH', 100);
+        super(scene, x, y, 'LeakSpriteSheet', 'PIPE_WRENCH', 100,  { start: 0, end: 34 }, { start: 0, end: 34, first: 1 }, 24);
     }
 }
 
