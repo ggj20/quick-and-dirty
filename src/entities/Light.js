7f1edd70f32bd8fa089a2630aba26e82c5436871
Akuryou <mail@akuryou.net>
2020-02-21 16:24:31 +0100

    use getAlphaLevel method to determine light strength

--- src/entities/Light.js
+++ src/entities/Light.js
@@ -12,7 +12,8 @@ class Light extends Phaser.GameObjects.Container {
     if(Math.random() < this.scene.game.state.voltage / 100) {
       this.shadow.alpha = 0;
     } else {
-      this.shadow.alpha = 1 - (this.scene.game.state.voltage / 100);
+      this.shadow.alpha = this.getAlphaLevel(this.scene.game.state.voltage);
+    }
     setTimeout(this.update.bind(this), 2000);
   }
 
