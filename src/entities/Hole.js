001a5c7a80f83beca5e092c9344c584c8580c64b
Akuryou <mail@akuryou.net>
2020-03-10 17:00:50 +0100

    rework damage constructer to handle physics inernally

--- src/entities/Hole.js
+++ src/entities/Hole.js
@@ -1,8 +1,8 @@
 import Damage from './Damage';
 
 class Hole extends Damage {
-    constructor(scene, x, y) {
-        super(scene, x, y, 'HoleSpriteSheet', 'HAMMER', 100,  { start: 0, end: 33, first: 0 }, { start: 33, end: 33 }, 17);
+    constructor(scene, x, y, group) {
+        super(scene, x, y, group, 'HoleSpriteSheet', 'HAMMER', 100,  { start: 0, end: 33, first: 0 }, { start: 33, end: 33 }, 17);
         scene.sound.play('HoleSound');
     }
 }
