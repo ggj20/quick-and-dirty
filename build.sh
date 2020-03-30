b0700db2eba9517ab5d600e4de34ee96351bdb61
Richard <richard@stromer.org>
2020-03-30 17:42:20 +0200

    add date to builds

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 rm -rf dist
-rm -f quick-and-dirty.zip
+rm -f quick-and-dirty*.zip
 
 yarn build
 cp -r {controller,airconsole-controls,controller.html} dist
 
-zip -r quick-and-dirty.zip dist
+zip -r "quick-and-dirty-$(date +"%Y-%m-%d-%H-%M-%S").zip" dist
