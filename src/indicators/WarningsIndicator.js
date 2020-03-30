d8779cb57188301a1e574f804547d1ad136afd1e
Richard <richard@stromer.org>
2020-03-30 18:18:23 +0200

    display warning indicator icons

--- src/indicators/WarningsIndicator.js
+++ src/indicators/WarningsIndicator.js
@@ -1,10 +1,21 @@
 import countDamagesByToolType from '../utils/count-damages-by-tool-type';
 
+const offsetX = 10;
+const offsetY = 40;
+const sizeY = 150;
+
+const getPositionY = index => offsetY + index * sizeY;
+
 export default class WarningsIndicator extends Phaser.GameObjects.Container {
   constructor(scene, x, y) {
     super(scene, x, y);
     this.scene = scene;
     scene.add.existing(this);
+
+    this.scene.add.image(offsetX, getPositionY(1), 'WarningAltitudeIcon');
+    this.scene.add.image(offsetX, getPositionY(2), 'WarningElectricityIcon');
+    this.scene.add.image(offsetX, getPositionY(3), 'WarningFireIcon');
+    this.scene.add.image(offsetX, getPositionY(4), 'WarningSteamIcon');
 
     this.update();
     setInterval(() => this.update(), 500);
