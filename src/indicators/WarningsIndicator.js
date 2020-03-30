ee9c3e469cbe8bee9faff36f788fff5f83af0e7e
Richard <richard@stromer.org>
2020-03-30 18:33:09 +0200

    tint damage indicators and display if there are multiple damages

--- src/indicators/WarningsIndicator.js
+++ src/indicators/WarningsIndicator.js
@@ -12,10 +12,30 @@ export default class WarningsIndicator extends Phaser.GameObjects.Container {
     this.scene = scene;
     scene.add.existing(this);
 
-    this.scene.add.image(offsetX, getPositionY(1), 'WarningAltitudeIcon');
-    this.scene.add.image(offsetX, getPositionY(2), 'WarningElectricityIcon');
-    this.scene.add.image(offsetX, getPositionY(3), 'WarningFireIcon');
-    this.scene.add.image(offsetX, getPositionY(4), 'WarningSteamIcon');
+    this.warningIndicatorAltitude = this.scene.add.image(
+      offsetX,
+      getPositionY(1),
+      'WarningAltitudeIcon',
+    );
+    this.warningIndicatorAltitude.tint = 0xff0000;
+    this.warningIndicatorElectricity = this.scene.add.image(
+      offsetX,
+      getPositionY(2),
+      'WarningElectricityIcon',
+    );
+    this.warningIndicatorElectricity.tint = 0xff0000;
+    this.warningIndicatorFire = this.scene.add.image(
+      offsetX,
+      getPositionY(3),
+      'WarningFireIcon',
+    );
+    this.warningIndicatorFire.tint = 0xff0000;
+    this.warningIndicatorSteam = this.scene.add.image(
+      offsetX,
+      getPositionY(4),
+      'WarningSteamIcon',
+    );
+    this.warningIndicatorSteam.tint = 0xff0000;
 
     this.update();
     setInterval(() => this.update(), 500);
@@ -23,11 +43,15 @@ export default class WarningsIndicator extends Phaser.GameObjects.Container {
 
   update() {
     const { voltage, altitude } = this.scene.game.state;
-
     const fireCount = countDamagesByToolType(this.scene, 'EXTINGUISHER');
     const holeCount = countDamagesByToolType(this.scene, 'HAMMER');
     const leakCount = countDamagesByToolType(this.scene, 'PIPE_WRENCH');
     const sparkCount = countDamagesByToolType(this.scene, 'SOLDERING_IRON');
+
+    this.warningIndicatorAltitude.alpha = holeCount - 3;
+    this.warningIndicatorElectricity.alpha = sparkCount - 3;
+    this.warningIndicatorFire.alpha = fireCount - 5;
+    this.warningIndicatorSteam.alpha = leakCount - 3;
 
     console.log('---');
     console.log('fire', fireCount);
